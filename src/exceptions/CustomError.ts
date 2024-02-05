5599388a4301883add566edd187cac2e7354df73
Igwe Chinonso <igwechinonso77@gmail.com>
2024-02-05 17:34:21 +0100

    refactor

--- src/exceptions/CustomError.ts
+++ src/exceptions/CustomError.ts
@@ -5,7 +5,7 @@ export class CustomError extends Error {
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "Internal Server Error";
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
